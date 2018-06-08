0d5c355b31f920059aef96c90c5778f7ab058c04
mingyu <rlaalsrbgk@gmail.com>
2018-06-08 16:39:25 +0900

    choices, verbose_name, help_text실습

--- app/models/fields/models.py
+++ app/models/fields/models.py
@@ -7,5 +7,5 @@ class Person(models.Model):
         ('M', 'Medium'),
         ('L', 'Large'),
     )
-    name = models.CharField(max_length=60)
-    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+    name = models.CharField( max_length=60)
+    shirt_size = models.CharField('셔츠사이즈', help_text='s는 작습니다', max_length=1, choices=SHIRT_SIZES)
