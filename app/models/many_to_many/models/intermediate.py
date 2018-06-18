385c5882c66843ded40e56c63ae0f05aa0752997
mingyu <rlaalsrbgk@gmail.com>
2018-06-18 14:31:23 +0900

    many_to_many.models package화

--- app/models/many_to_many/models/intermediate.py
+++ app/models/many_to_many/models/intermediate.py
@@ -1,23 +1,10 @@
 from django.db import models
 
-
-class Topping(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
-
-
-class Pizza(models.Model):
-    name = models.CharField(max_length=50)
-    topping = models.ManyToManyField(
-        Topping,
-        related_name='pizzas',
-    )
-
-    def __str__(self):
-        return self.name
-
+__all__ = (
+    'Person',
+    'Group',
+    'Membership',
+)
 
 class Person(models.Model):
     name = models.CharField(max_length=128)
