51f2e53a24218dbc41310de8af177c43e7132f3f
mingyu <rlaalsrbgk@gmail.com>
2018-06-18 15:08:13 +0900

    show_friends() 구현

--- app/models/many_to_many/models/self.py
+++ app/models/many_to_many/models/self.py
@@ -14,3 +14,10 @@ class FacebookUser(models.Model):
 
     def __str__(self):
         return self.name
+
+    def show_friends(self):
+        print('{}의 친구목록'.format(self.name))
+        for friend in self.friends.all():
+            print('-{}'.format(friend))
+
+        print('총 {}명'.format(len(self.friends.all())))
